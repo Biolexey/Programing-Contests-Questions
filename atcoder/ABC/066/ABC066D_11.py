bb6f03a98d8577267dd8323617f94b1042af8796
RevB <biolexey@gmail.com>
2020-11-11 11:15:59 +0900

    11/11

--- atcoder/ABC/066/ABC066D_11.py
+++ atcoder/ABC/066/ABC066D_11.py
@@ -1,20 +1,36 @@
+from collections import Counter
 n = int(input())
 num = [*map(int, input().split())]
 mod = 10**9+7
-d = 0
-for i in range(1, n+1):
-    if num.count(i) == 2:
-        d = i
-        break
-dup = [i for i, x in enumerate(num) if x == d]
-l, r = dup[0]+1, dup[1]
 
-lim = 1000
-fac = [1]*(lim+1)
-for i in range(1, lim+1):
-    fac[i] = (fac[i-1]*i)%mod
+#リストを用いて重複している数字のインデックスを取ろうと思ったがTLE
+# d = 0
+# for i in range(1, n+1):
+#     if num.count(i) == 2:
+#         d = i
+#         break
+# dup = [i for i, x in enumerate(num) if x == d]
+# l, r = dup[0]+1, dup[1]
 
-print(fac)
+ctr = Counter(num)#Counterを用いて重複数字を抽出
+dup = ctr.most_common()[0][0]
+l, r = num.index(dup)+1, n-num[::-1].index(dup)
+
+lim = n+9
+fac = [1, 1] + [0]*lim
+inv = [0, 1] + [0]*lim
+finv = [1, 1] + [0]*lim
+for i in range(2, lim+1):
+    fac[i] = fac[i-1]*i%mod
+    inv[i] = -inv[mod%i]*(mod//i)%mod
+    finv[i] = finv[i-1]*inv[i]%mod
+
+def comb(n, r):
+    if n < r:
+        return 0
+    if n < 0 or r < 0:
+        return 0
+    return fac[n]*(finv[r]*finv[n-r]%mod)%mod
 
 for k in range(1, n+2):
-    print(fac[n+1]//(fac[k]*fac[n+1-k]) - fac[l-1+n-r]//(fac[k-1]*fac[l+n-r-k]))
+    print((comb(n+1, k) - comb(l-1+n-r, k-1))%mod)
