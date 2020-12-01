6a32e0cf50495a8f8e9a8ea02df1762047d5ac46
RevB <biolexey@gmail.com>
2020-12-02 01:35:04 +0900

    12/2

--- atcoder/ABC/80/080/ABC080C_ShoppingStreet.py
+++ atcoder/ABC/80/080/ABC080C_ShoppingStreet.py
@@ -2,4 +2,3 @@ n = int(input())
 f = [int(input().replace(" ", ""), 2) for _ in range(n)]
 p = [[*map(int, input().split())] for _ in range(n)]
 print(max(sum([p[bin(f&i).count("1")] for f, p in zip(f, p)]) for i in range(1, 2**10)))
-#test
