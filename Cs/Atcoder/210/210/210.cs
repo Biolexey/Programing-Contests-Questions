70b9ef129cde8d38b17c96919f3c8fd36495ef58
Biolexey <Biolexey@gmail.com>
2022-05-06 17:00:08 +0900

    5/6

---
+++ Cs/Atcoder/210/210/210.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace ConsoleApp210
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ipt = Console.ReadLine().Split().Select(int.Parse).ToArray();//var iptでも可
+            //安くなる金額
+            var discount = ipt[2] - ipt[3];
+            //追加購入量
+            var moreCnt = ipt[0] - ipt[1];
+            //追加購入しない場合
+            if(moreCnt <= 0)
+            {
+                Console.WriteLine(ipt[2] * ipt[0]);
+            }
+            else
+            {
+                Console.WriteLine(ipt[2] * ipt[0] - discount * moreCnt);
+            }
+         
+        }
+ 
+    }
+}
---
+++ Cs/Atcoder/210/211/211.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace ConsoleApp210
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            var num = Console.ReadLine().Split();
+            double a = double.Parse(num[0]);
+            double b = double.Parse(num[1]);
+
+            c = (a-b) / 3 + b;
+            Console.WriteLine(c);
+        }
+ 
+    }
+}
