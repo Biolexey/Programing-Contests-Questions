ba5de590d76887703ed6420a6113282108c71dd4
Biolexey <Biolexey@gmail.com>
2022-05-07 18:11:42 +0900

    5/7

---
+++ Cs/Atcoder/210/212/212.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class prob{
+    static void Main(){
+        var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0], b = ab[1];
+        string ans;
+
+        if(a==0)ans = "Silver";
+        else if(b==0)ans = "Gold";
+        else ans = "Alloy";
+
+        Console.WriteLine(ans);
+    }
+    
+}
