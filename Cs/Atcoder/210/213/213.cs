6397dde35b509f9ba6c523c73de880d985779e7a
Biolexey <Biolexey@gmail.com>
2022-05-09 00:04:36 +0900

    5/9

---
+++ Cs/Atcoder/210/213/213.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class prob{
+    static void Main(){
+        var ab = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0], b = ab[1];
+        int ans = 0;
+        for(int i = 0; i < 256; i++){
+            if((a^i) == b){
+                ans = i;
+                break;
+            }
+        }
+        Console.WriteLine(ans);
+    }
+}
