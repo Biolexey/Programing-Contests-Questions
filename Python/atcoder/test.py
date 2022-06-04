a681525f0fbd9de8413ad92f3faa11f7a1dd798d
Biolexey <Biolexey@gmail.com>
2022-06-04 17:10:27 +0900

    6/4

--- Python/atcoder/test.py
+++ Python/atcoder/test.py
@@ -1,13 +1,44 @@
-def make_divisors(n):                           # 約数関数
-    lower_divisors , upper_divisors = [], []
-    i = 1
-    while i*i <= n:
-        if n % i == 0:
-            lower_divisors.append(i)
-            if i != n // i:
-                upper_divisors.append(n//i)
-        i += 1
-    upper_divisors.reverse()
-    return lower_divisors + [i for i in upper_divisors if i <= n//2]
+import sys
+import requests
+import pprint
+import json
 
-print(make_divisors(int(input())))
+global url
+url = "http://challenge-server.code-check.io/api/recursive/ask"
+
+
+
+def func(n):
+    global url, txt
+
+    N = [0 for _ in range(51)]
+
+    if n == 0:
+        return 1
+    elif n == 2:
+        return 2
+    elif n % 2 == 0:
+        return func(n-1) + func(n-2) + func(n-3) + func(n-4)
+    else:
+        if N[n]:
+            return N[n]
+        else:
+            param = {"seed": txt, "n": n}
+            res = requests.get(url, params=param)
+            data = res.json()
+            result = int(data["result"])
+            N[n] = result
+            return result
+
+if __name__ == '__main__':
+    try:
+        global txt
+        args = sys.argv
+        if len(args) > 3:
+            main()
+        txt, num = args[1], int(args[2])
+        print(func(num))
+
+    except:
+        print("error!")
+        main()
